52da8301732d384cae25ed05c9d3bf6c4535734c
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-16 11:25:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,17 +3,17 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-BRANDS = []
+  BRANDS = []
 
-def initialize(brand)
-  @brand = brand
-end
+  def initialize(brand)
+    @brand = brand
+  end
 
-def brand=(brands)
-  @brands = brands
-  if ! (BRANDS.include?(@brand))
-  BRANDS << brands
-end
+  def brand=(brands)
+    @brands = brands
+    if ! (BRANDS.include?(@brand))
+    BRANDS << brands
+  end
 end
 
 end
