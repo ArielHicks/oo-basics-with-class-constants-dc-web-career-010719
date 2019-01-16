f5faf05e7d87ea5e8c1b103af68394bb0efd7608
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-16 11:19:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,7 +9,10 @@ def initialize(title)
   @title = title
 end
 
-def 
+def genre=(genre)
+  @genre = genre 
+  GENRES << genre 
+end
 
 def turn_page
   puts "Flipping the page...wow, you read fast!"
